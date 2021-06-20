46122da6f75091521c91da852ff6bfb3ea18659c
wendey <wendeyricardo@gmail.com>
2021-06-20 15:26:44 -0300

    criado o controller, o model e a table diarista

---
+++ app/Http/Controllers/DiaristaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diarista;
+use Illuminate\Http\Request;
+
+class DiaristaController extends Controller
+{
+    public function index()
+    {
+        $diaristas = Diarista::get();
+
+        return view('index', [
+            'diaristas' => $diaristas
+        ]);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Diaristas</title>
+</head>
+<body>
+    <h1>Página inicial E-Diaristas</h1>
+
+    {{ dd($diaristas) }}
+</body>
+</html>
