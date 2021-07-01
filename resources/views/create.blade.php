2468b7b8788ed352d24437c56cacafac9413e089
wendey <wendeyricardo@gmail.com>
2021-07-01 16:09:41 -0300

    ajuste para criar 'template'

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,38 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Criar Diarista</title>
-  </head>
-  <body>
-  <header><nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('diaristas.index')}}">E-Diaristas</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('diaristas.index')}}">Lista de Diaristas</a>
-        </li>       
-        
-      </ul>
-      
-    </div>
-  </div>
-</nav> 
-  </header>
+@section('titulo', 'Criar Diaristal')
     
- 
-    <div class='container'>
-        <h1>Criar Diarista</h1>
+@section('conteudo')
+
+<h1>Criar Diarista</h1>
         <form action="{{route('diaristas.store')}}" method="POST" enctype="multipart/form-data">
           @csrf
   
@@ -93,17 +65,4 @@
 
 </form>
     
-    </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,77 +1,38 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Página Inicial</title>
-  </head>
-  <body>
-  <header><nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('diaristas.index')}}">E-Diaristas</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('diaristas.index')}}">Lista de Diaristas</a>
-        </li>       
-        
-      </ul>
-      
-    </div>
-  </div>
-</nav> 
-  </header>
+@section('titulo', 'Página Inicial')
     
+@section('conteudo')
 
-    <div class='container'>
-    <h1>Lista de Diaristas</h1>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Telefone</th>
-      <th scope="col">Ações</th>
-    </tr>
-  </thead>
-  <tbody>
-    @forelse ($diaristas as $diaristas)
-    <tr>
-      <th scope="row">{{$diaristas-> id}}</th>
-      <td>{{$diaristas-> nome_completo}}</td>
-      <td>{{$diaristas-> telefone}}</td>
-      <td></td>
-    </tr>
-    @empty
-    <tr>
-    <th></th>
-    <td>Nenhum registro cadastrado</td>
-    <td></td>
-    <td></td>
-    </tr>
-    @endforelse    
-  </tbody>
+<h1>Lista de Diaristas</h1>
+<table class="table">
+<thead>
+<tr>
+  <th scope="col">ID</th>
+  <th scope="col">Nome</th>
+  <th scope="col">Telefone</th>
+  <th scope="col">Ações</th>
+</tr>
+</thead>
+<tbody>
+@forelse ($diaristas as $diaristas)
+<tr>
+  <th scope="row">{{$diaristas-> id}}</th>
+  <td>{{$diaristas-> nome_completo}}</td>
+  <td>{{$diaristas-> telefone}}</td>
+  <td></td>
+</tr>
+@empty
+<tr>
+<th></th>
+<td>Nenhum registro cadastrado</td>
+<td></td>
+<td></td>
+</tr>
+@endforelse    
+</tbody>
 </table>
 <a href="{{route('diaristas.create')}}" class="btn btn-success">Nova Diarista</a>
-    </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+    
+@endsection
